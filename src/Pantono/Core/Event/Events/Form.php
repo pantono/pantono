821226d233dfdf0fd6539f2d72d75cefcf3e6a8e
Chris Burton <csburton@gmail.com>
2015-04-27 12:23:24 +0100

    Removing name from event as not required

--- src/Pantono/Core/Event/Events/Form.php
+++ src/Pantono/Core/Event/Events/Form.php
@@ -4,24 +4,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class Form extends General
 {
-    private $name;
     private $builder;
-
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
 
     /**
      * @param FormBuilderInterface $builder
