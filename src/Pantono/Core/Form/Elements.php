133f70a9ca6367aad8d82af45c64562c54ce4df8
Chris Burton <csburton@gmail.com>
2015-04-27 13:35:03 +0100

    Adding abstract function

--- src/Pantono/Core/Form/Elements.php
+++ src/Pantono/Core/Form/Elements.php
@@ -1,9 +1,21 @@
 <?php namespace Pantono\Core\Form;
-
+use Symfony\Component\Form\FormBuilderInterface;
 trait Elements
 {
+
+    /**
+     * @return FormBuilderInterface
+     */
+    abstract public function getBuilder();
+
+    /**
+     * @param $name
+     * @param array $classes
+     * @param $options
+     */
     public function addTextField($name, $classes = [], $options)
     {
+
         $options['attr']['class'] .= $classes;
         $this->getBuilder()->add(
             $name,
