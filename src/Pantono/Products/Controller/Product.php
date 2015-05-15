dccd66753b4da9c64f35c653b2f3775b7c0b9291
Chris Burton <csburton@gmail.com>
2015-05-15 16:13:04 +0100

    Adding in new line chars

---
+++ src/Pantono/Products/Controller/Product.php
@@ -0,0 +1,22 @@
+<?php namespace Pantono\Products\Controller;
+
+use Pantono\Core\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class Product extends Controller
+{
+    public function listAction(Request $request)
+    {
+
+    }
+
+    public function addAction(Request $request)
+    {
+
+    }
+
+    public function editAction(Request $request)
+    {
+        
+    }
+}
