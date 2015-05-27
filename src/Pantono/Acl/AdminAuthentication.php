d7b8c664755cbadf1af085620b51f3984bb44aea
Chris Burton <csburton@gmail.com>
2015-05-27 14:28:31 +0100

    Populating blank method

--- src/Pantono/Acl/AdminAuthentication.php
+++ src/Pantono/Acl/AdminAuthentication.php
@@ -67,7 +67,11 @@ class AdminAuthentication
 
     public function deleteAdminUserById($id)
     {
-
+        $user = $this->getSingleUser($id);
+        if (!$user) {
+            return false;
+        }
+        return $this->deleteAdminUser($user);
     }
 
     public function deleteAdminUser(AdminUser $user)
