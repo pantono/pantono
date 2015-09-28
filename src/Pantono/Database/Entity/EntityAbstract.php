500b92cd145e3d69c54d4a5b0567b855ea637881
Chris Burton <csburton@gmail.com>
2015-09-28 21:05:58 +0100

    Adding missing file

---
+++ src/Pantono/Database/Entity/EntityAbstract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pantono\Database\Entity;
+
+/**
+ * Class which all entities should extend
+ *
+ * @package Pantono\Database\Entity
+ * @author  Chris Burton <csburton@gmail.com>
+ */
+abstract class EntityAbstract
+{
+
+}
